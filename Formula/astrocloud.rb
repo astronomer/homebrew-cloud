937b143f61e314a778ffbb9c22cf5a18db05c103
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-14 23:52:22 -0400

    Brew formula update for cloud-cli version v1.4.0

--- Formula/astrocloud.rb
+++ Formula/astrocloud.rb
@@ -10,7 +10,7 @@ class Astrocloud < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://goreleaserdev.blob.core.windows.net/goreleaser-test-container/releases/v1.4.0/cloud-cli_1.4.0_Darwin_arm64.tar.gz"
-      sha256 "a543fcefa7245922ff38ae3247e79a94c0131ab6f19434e1736d1a0e2a4b204b"
+      sha256 "82515ee6b57059861bb28e83ab6d8c036d0987cd938e99b181f6aff3a3927310"
 
       def install
         bin.install "astrocloud"
@@ -18,7 +18,7 @@ class Astrocloud < Formula
     end
     if Hardware::CPU.intel?
       url "https://goreleaserdev.blob.core.windows.net/goreleaser-test-container/releases/v1.4.0/cloud-cli_1.4.0_Darwin_x86_64.tar.gz"
-      sha256 "2e21445cef9e80e0ad5bc799ce98ad62753b269c186e2a87416051cb43022d25"
+      sha256 "15e7ab9251eccff6244fc436f952e03c35277f17a2a017001e22038256153313"
 
       def install
         bin.install "astrocloud"
@@ -29,7 +29,7 @@ class Astrocloud < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://goreleaserdev.blob.core.windows.net/goreleaser-test-container/releases/v1.4.0/cloud-cli_1.4.0_Linux_arm64.tar.gz"
-      sha256 "0b8de618dd96371803a377b151a4f519f7384bbca3969d651f851c9685a271f8"
+      sha256 "992911e2a974744e0c6911108d765438a876335d40cd644e1ec18307dcbfa8d4"
 
       def install
         bin.install "astrocloud"
@@ -37,7 +37,7 @@ class Astrocloud < Formula
     end
     if Hardware::CPU.intel?
       url "https://goreleaserdev.blob.core.windows.net/goreleaser-test-container/releases/v1.4.0/cloud-cli_1.4.0_Linux_x86_64.tar.gz"
-      sha256 "82e326838f85a41c7967fab5e9bac9261f179e12f9149e52b61630ad2c20dd0c"
+      sha256 "f8e761a434f1d3ae7d8d93d27dd3ed91a516dc5cde15862c3c8b35aaaff5b29c"
 
       def install
         bin.install "astrocloud"
